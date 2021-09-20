6f0eb76ec3dde1b9714e9c0c2cb0654da88a3ea2
PoalChaynik <alpolaop13@gmail.com>
2021-09-20 13:50:40 +0300

    modified:   numbers.py

--- numbers.py
+++ numbers.py
@@ -1,1 +1,23 @@
+num1 = 1 #int
+num2 = 3.5 #float
 
+sum = num1 + num2
+print(str(num1) + "plus" + str(num2) + "Vienads" + str(sum))
+
+print(num1, "plus", num2, "vienads", sum)
+
+#ievade
+
+num1 = int(input("1 skaitlis: "))
+num2 = int(input("2 skaitlis: "))
+
+sum = num1+num2
+
+print(sum)
+
+a = "chocolate"
+b = "TOKYO REVENGERS!"
+
+print("i like %s ice cream" %a)
+print("i am {0} years old and my name is {1}" .format("15","Aleksis"))
+print(f"I LOVE {b}")
