15d7eb5536d5ff6f34a26d1325339ad304818793
PoalChaynik <alpolaop13@gmail.com>
2021-09-21 13:54:46 +0300

    new file:   TnS.py

---
+++ TnS.py
@@ -0,0 +1,19 @@
+#Tuples
+
+# ()
+
+newTuple = ('skola','majas','pagrabs')
+print(newTuple)
+print(len(newTuple)) #elementu daudzums
+
+print(newTuple[1]) #piekluve elementam
+
+print(newTuple.index('majas')) #elementa numuru dabut var
+
+newTuple = (1,1,0,0,1,0,1,1,0,1,0,0,1,1,0,1,0,0,1)
+print(newTuple.count(0)) #saskaita cik 0 ir tupele
+
+#Set
+
+# {}
+
