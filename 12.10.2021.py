d4c7df10b0bf5bbfd11d7b2866b13e93c4bb09e5
PoalChaynik <alpolaop13@gmail.com>
2021-10-12 13:40:42 +0300

    modified:   12.10.2021.py

--- 12.10.2021.py
+++ 12.10.2021.py
@@ -8,3 +8,13 @@ def atgriez_skaitli(skaitlis1,skaitlis2):
                 return max(skaitli)
 
 print(atgriez_skaitli(6,2))
+
+def sakas_vienadi(vards):
+    vards = vards.lower().split()
+    if vards[0][0] == vards[1][0]:
+        return True
+    else: 
+        return False
+
+print(sakas_vienadi('Maza Lama'))
+print(sakas_vienadi('Liela Lama'))
