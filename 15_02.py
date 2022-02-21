d1fabb110e2b96cb7e3e19872260f34aa4ad0ae0
PoalChaynik <alpolaop13@gmail.com>
2022-02-21 12:51:58 +0200

    strada

--- 15_02.py
+++ 15_02.py
@@ -11,8 +11,8 @@
 import json
 vardss = input('ievadiet vardu: ')
 uzvardss = input('ievadiet uzvardu: ')
-vecumss = input('ievadiet vecumu: ')
-tell = input('ievadiet telefona numuru: ')
+vecumss = int(input('ievadiet vecumu: '))
+tell = int(input('ievadiet telefona numuru: '))
 
 vardnica = {
     'Vards':vardss,
@@ -21,8 +21,19 @@ vardnica = {
     'Telefona Numurs':tell
 }
 
+jauni_dati = []
+
 def dati(vards,uzvards,vecums,tel):
+    with open('ievaktieDati.json','r',encoding='utf-8') as file:
+        a = json.load(file)
+        jauni_dati.append(vardnica)
+        jauni_dati.append(a)
+        print(jauni_dati)
     with open('ievaktieDati.json','w',encoding='utf-8') as file:
-        json.dump(vardnica,file,indent=4,ensure_ascii=False)
+        json.dump(jauni_dati,file,indent=4,ensure_ascii=False)
+
+
+    
+
 
 dati(vardss,uzvardss,vecumss,tell)
