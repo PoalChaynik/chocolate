18e742982d84e05fba1e89fd541754b079e7ef57
PoalChaynik <alpolaop13@gmail.com>
2022-02-22 12:25:05 +0200

    ee

--- 15_02.py
+++ 15_02.py
@@ -15,25 +15,26 @@ vecumss = int(input('ievadiet vecumu: '))
 tell = int(input('ievadiet telefona numuru: '))
 
 vardnica = {
-    'Vards':vardss,
-    'Uzvards':uzvardss,
-    'Vecums':vecumss,
-    'Telefona Numurs':tell
+    "Uzvārds":uzvardss,
+    "Vecums":vecumss,
+    "Telefona numurs":tell
 }
 
-jauni_dati = []
+with open("ievaktieDati.json","r", encoding="utf-8") as fails:
+    json_data = json.load(fails)
 
-def dati(vards,uzvards,vecums,tel):
-    with open('ievaktieDati.json','r',encoding='utf-8') as file:
-        a = json.load(file)
-        jauni_dati.append(vardnica)
-        jauni_dati.append(a)
-        print(jauni_dati)
-    with open('ievaktieDati.json','w',encoding='utf-8') as file:
-        json.dump(jauni_dati,file,indent=4,ensure_ascii=False)
+    ir_saraksta =False
+    for key in json_data.keys():
+        if key == vardss:
+            break
+        if key != vardss:
+            ir_saraksta = True
+
+    if ir_saraksta == False:
+        print("Vārds ir sarakstā")
+    else:
+        json_data[vardss]=vardnica
 
 
-    
-
-
-dati(vardss,uzvardss,vecumss,tell)
+with open("ievaktieDati.json","w", encoding="utf-8") as fails:
+    json.dump(json_data,fails, indent = 4, ensure_ascii=False)
