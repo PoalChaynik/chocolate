f16109b9677d06a0389186f196d7284298c66173
PoalChaynik <alpolaop13@gmail.com>
2022-05-02 13:51:23 +0300

    02.05.2022

--- 02.05.2022.py
+++ 02.05.2022.py
@@ -1,39 +1,53 @@
-from unittest import skip
+# uzdevums
 
+# x = 5
+# print(x)
 
-x = 5
-print(x)
+# skaitlis = 7
+# if(skaitlis > 7):
+#     print("Labinieks")
 
-skaitlis = 7
-if(skaitlis > 7):
-    print("Labinieks")
+# skaitlis = 10000
+# a = skaitlis / 2
+# print(a)
 
-skaitlis = 10000
-a = skaitlis / 2
-print(a)
-
-a = [1, 2, 3]
-print ("Otrais elements = %d" %(a[1]))
+# a = [1, 2, 3]
+# print ("Otrais elements = %d" %(a[1]))
  
-print ("Trešais elements = %d" %(a[2]))
+# print ("Trešais elements = %d" %(a[2]))
 
-def funkcija(a):
-    if a < 4:
-        if a-3 != 0:
-            b = a/(a-3)
-            print("Value of b = ", b)
+# def funkcija(a):
+#     if a < 4:
+#         if a-3 != 0:
+#             b = a/(a-3)
+#             print("Value of b = ", b)
     
-funkcija(3)
-funkcija(5)
+# funkcija(3)
+# funkcija(5)
 
-x = ("four")
-print(x)
+# x = ("four")
+# print(x)
 
-a = 'viens'
-b = 'divi'
-print('%s,%s'%(a,b))
+# a = 'viens'
+# b = 'divi'
+# print('%s,%s'%(a,b))
 
-for i in [-2, -1, 0, 1, 2]:
-    if i == 0:
-        i += 1
-    print(1 / i)
+# for i in [-2, -1, 0, 1, 2]:
+#     if i == 0:
+#         i += 1
+#     print(1 / i)
+
+#Biblioteka sys
+
+import sys
+
+saraksts = ['a',0,2]
+
+for i in saraksts:
+    try:
+        print('Skaitlis ir:',i)
+        a = 1/int(i)
+        break
+    except Exception as e:
+        print('Kluda',e.__class__)
+print(a)
